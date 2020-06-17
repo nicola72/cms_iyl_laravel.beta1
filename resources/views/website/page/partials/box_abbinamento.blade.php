5311587d702d280b6f93b7a79be8d3d2de8b67a6
Nicola <nicola.tamburini@fjstudio.com>
2020-06-17 16:00:48 +0200

    commit

--- resources/views/website/page/partials/box_abbinamento.blade.php
+++ resources/views/website/page/partials/box_abbinamento.blade.php
@@ -26,7 +26,10 @@
                 <strong>{{ $pairing->{'desc_'.app()->getLocale()} }}</strong>
             </div>
 
-            <i>@lang('msg.codice_prodotto'): {{$pairing->product1->codice}} + {{$pairing->product2->codice}}</i>
+            <i>
+                @lang('msg.codice_prodotto'):<br>
+                {{$pairing->product1->codice}} + {{$pairing->product2->codice}}
+            </i>
             <a href="{{$pairing->url()}}" class="dettagli" >
                 <i class="fa fa-search"></i> @lang('msg.dettagli_prodotto')
             </a>
